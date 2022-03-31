a1c2bdf9425f20e46583e15ca7414a8d37c72840
Rehamsalha <reham20ms@gmail.com>
2022-03-31 16:05:35 +0300

    homework

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,61 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function(){
+$name = 'Mohammed';
+$age = 25;
+//$tasks=[
+    //'task 1',
+    //'task 2',
+    //'task 3'
+//];
+
+$tasks= [
+'1'=>'Task1',
+'2' =>'Task2',
+'3' => 'Task3'
+];
+
+
+
+
+
+
+//return view('about', [
+  //  'name' => $name,
+    //'age' => $age
+
+//]);
+
+//return view ('about') ->with('name' ,$name) ->with('age', $age);
+return view ('about', compact('tasks'));
+});
+
+Route::get('/task/{id}', function($id){
+    $tasks= [
+        '1'=>'Task1',
+        '2' =>'Task2',
+        '3' => 'Task3'
+        ];
+        $task = $tasks[$id];   
+        return view('task', compact('task'));
+});
+
+Route::get('/contact', function () {
+    return view('contact_us');
+});
+
+Route::post('/contact', function () {
+
+    $name =$_POST['name'];
+    return view('contact_us', compact('name'));
+});
+
+Route::get('/reham', function () {
+    return view('rehamss');
+});
+Route::post('/reham', function () {
+    $name =$_POST['name'];
+    return view('contact_us', compact('name'));
+});
